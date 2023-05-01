61e07b4ca88f05f52e6d87e46b545e229f8065d2
lisalindqvist95 <lindqvist.lisa@hotmail.com>
2023-05-01 10:04:13 +0000

    Display filtered list of posts

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
   const currentUser = useCurrentUser();
-  const profile_id = currentUser?.profile_id || "";
+  const profile_id = currentUser?.profile_id || ""; 
 
   return (
     <div className={styles.App}>
