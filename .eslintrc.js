b945d0124d8a92e7c6edd245dd51708c10e6076c
lisalindqvist95 <lindqvist.lisa@hotmail.com>
2023-06-29 03:00:31 +0000

    Try to fix error with deployement

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true
     },
     "extends": [
         "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
         "plugin:react/recommended"
     ],
     "overrides": [
@@ -20,11 +22,13 @@ module.exports = {
             }
         }
     ],
+    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
     "plugins": [
+        "@typescript-eslint",
         "react"
     ],
     "rules": {
